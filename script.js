a023c100da74651368fe800050224ec4c1d43d2d
0267BLOOD <kusmara307@gmail.com>
2026-09-26 19:27:23 +0700

    Add theme toggle functionality to script.js

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const pesan = document.querySelector("#pesan");
 tombol.addEventListener("click", function() {
     pesan.textContent = "Halo! Senang kenalan dengan kamu 👋";
 });
+
+
+const themeBtn = document.querySelector("#themeBtn");
+
+themeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        themeBtn.textContent = "🌙";
+    } else {
+        themeBtn.textContent = "☀️";
+    }
+});
